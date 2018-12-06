05e90b793a2c571a95d74d238d069cabff5e2d9e
IrisHU <huxueiris@163.com>
2018-12-06 09:49:40 +0100

    change pre-trained model

--- loading_model.py
+++ loading_model.py
@@ -27,8 +27,10 @@ class NnModel(object):
         utils.download(VGG_DOWNLOAD_LINK, VGG_FILENAME, EXPECTED_BYTES)
         self.vgg = scipy.io.loadmat(VGG_FILENAME)['layers']
         self.mean_pixels = np.array([123.68, 116.779, 103.939]).reshape((1, 1, 1, 3))
+        self.t = 0
 
     def __vgg_weights(self, lyr_idx, lyr_name):
+        print(self.vgg[0][lyr_idx][0][0][1][0])
         w = self.vgg[0][lyr_idx][0][0][2][0][0]
         b = self.vgg[0][lyr_idx][0][0][2][0][1]
         o_dim = int(w.shape[-1] / 2)
@@ -71,6 +73,11 @@ class NnModel(object):
         print(lyr_name)
         print(w.shape)
         print(out.shape)
+        a = w.shape.as_list()
+        d = a[0] * a[1] * a[2] * a[3]
+        self.t += d
+        print(d)
+        print(self.t)
         setattr(self, (stream_name + '_' + lyr_name), out)
 
     def fully_connected_layer(self, pre_lyr, out_dims, lyr_name, last_lyr=False):
@@ -89,6 +96,11 @@ class NnModel(object):
         print(lyr_name)
         print(w.shape)
         print(out.shape)
+        a = w.shape.as_list()
+        d = a[0]*a[1]*a[2]*a[3]
+        self.t += d
+        print(d)
+        print(self.t)
         setattr(self, lyr_name, out)
 
     def maxpool(self, prev_lyr, lyr_name, stream_name='d'):
@@ -140,49 +152,49 @@ class NnModel(object):
         self.conv2d_tanh(self.s_conv2_1, 7, 64, 'conv2_2', stream_name='s')
         self.attention_layer(self.d_conv2_2, self.s_conv2_2, 'd_conv2_2', 's_conv2_2')
         self.avgpool(self.atten_conv2_2, 'pool2')
-       # self.avgpool(self.s_conv2_2, 'pool2', stream_name='s')
+        self.avgpool(self.s_conv2_2, 'pool2', stream_name='s')
         self.dropout_layer(self.d_pool2)
 
-       #self.conv2d_tanh(self.d_pool2, 10, 64, 'conv3_1')
-       # self.conv2d_tanh(self.d_conv3_1, 12, 64, 'conv3_2')
+        self.conv2d_tanh(self.d_pool2, 10, 64, 'conv3_1')
+        self.conv2d_tanh(self.d_conv3_1, 12, 64, 'conv3_2')
        # self.conv2d_tanh(self.d_conv3_2, 14, 'conv3_3')
        # self.conv2d_tanh(self.d_conv3_3, 16, 'conv3_4')
-       # self.conv2d_tanh(self.s_pool2, 10, 64, 'conv3_1', stream_name='s')
-       # self.conv2d_tanh(self.s_conv3_1, 12, 64, 'conv3_2', stream_name='s')
+        self.conv2d_tanh(self.s_pool2, 10, 64, 'conv3_1', stream_name='s')
+        self.conv2d_tanh(self.s_conv3_1, 12, 64, 'conv3_2', stream_name='s')
        # self.conv2d_tanh(self.s_conv3_2, 14, 'conv3_3', stream_name='s')
        # self.conv2d_tanh(self.s_conv3_3, 16, 'conv3_4', stream_name='s')
-       # self.attention_layer(self.d_conv3_2, self.s_conv3_2, 'd_conv3_4', 's_conv3_4')
-       # self.avgpool(self.atten_conv3_4, 'pool3')
-        #self.avgpool(self.s_conv3_4, 'pool3', stream_name='s')
-       # self.dropout_layer(self.d_pool3)
+        self.attention_layer(self.d_conv3_2, self.s_conv3_2, 'd_conv3_2', 's_conv3_2')
+        self.avgpool(self.atten_conv3_2, 'pool3')
+        self.avgpool(self.s_conv3_2, 'pool3', stream_name='s')
+        self.dropout_layer(self.d_pool3)
 
-        # self.conv2d_tanh(self.d_pool3, 19, 'conv4_1')
-        # self.conv2d_tanh(self.d_conv4_1, 21, 'conv4_2')
+        self.conv2d_tanh(self.d_pool3, 17, 64, 'conv4_1')
+        self.conv2d_tanh(self.d_conv4_1, 19, 64, 'conv4_2')
         # self.conv2d_tanh(self.d_conv4_2, 23, 'conv4_3')
         # self.conv2d_tanh(self.d_conv4_3, 25, 'conv4_4')
-        # self.conv2d_tanh(self.s_pool3, 19, 'conv4_1', stream_name='s')
-        # self.conv2d_tanh(self.s_conv4_1, 21, 'conv4_2', stream_name='s')
+        self.conv2d_tanh(self.s_pool3, 17, 64, 'conv4_1', stream_name='s')
+        self.conv2d_tanh(self.s_conv4_1, 19, 64, 'conv4_2', stream_name='s')
         # self.conv2d_tanh(self.s_conv4_2, 23, 'conv4_3', stream_name='s')
         # self.conv2d_tanh(self.s_conv4_3, 25, 'conv4_4', stream_name='s')
-        # self.attention_layer(self.d_conv4_4, self.s_conv4_4, 'd_conv4_4', 's_conv4_4')
-        # self.avgpool(self.d_conv4_4, 'pool4')
-        # self.avgpool(self.s_conv4_4, 'pool4', stream_name='s')
+        self.attention_layer(self.d_conv4_2, self.s_conv4_2, 'd_conv4_2', 's_conv4_2')
+        self.avgpool(self.d_conv4_2, 'pool4')
+        #self.avgpool(self.s_conv4_2, 'pool4', stream_name='s')
         # self.dropout_layer(self.d_pool4)
         #
-        # self.conv2d_tanh(self.d_pool4, 28, 'conv5_1')
-        # self.conv2d_tanh(self.d_conv5_1, 30, 'conv5_2')
+        #self.conv2d_tanh(self.d_pool4, 24, 64, 'conv5_1')
+        #self.conv2d_tanh(self.d_conv5_1, 25, 64, 'conv5_2')
         # self.conv2d_tanh(self.d_conv5_2, 32, 'conv5_3')
         # self.conv2d_tanh(self.d_conv5_3, 34, 'conv5_4')
-        # self.conv2d_tanh(self.s_pool4, 28, 'conv5_1', stream_name='s')
-        # self.conv2d_tanh(self.s_conv5_1, 30, 'conv5_2', stream_name='s')
+        #self.conv2d_tanh(self.s_pool4, 24, 64, 'conv5_1', stream_name='s')
+        #self.conv2d_tanh(self.s_conv5_1, 25, 64, 'conv5_2', stream_name='s')
         # self.conv2d_tanh(self.s_conv5_2, 32, 'conv5_3', stream_name='s')
         # self.conv2d_tanh(self.s_conv5_3, 34, 'conv5_4', stream_name='s')
-        # self.attention_layer(self.d_conv5_4, self.s_conv5_4, 'd_conv5_4', 's_conv5_4')
-        # self.avgpool(self.d_conv5_4, 'pool5')
+        #self.attention_layer(self.d_conv5_2, self.s_conv5_2, 'd_conv5_2', 's_conv5_2')
+        #self.avgpool(self.d_conv5_2, 'pool5')
         # self.dropout_layer(self.d_pool5)
         # self.avgpool(self.s_conv5_4, 'pool5', stream_name='s')
 
-        self.fully_connected_layer(self.d_pool2, 64, 'fc6')
+        self.fully_connected_layer(self.d_pool4, 64, 'fc6')
         self.dropout_layer(self.fc6)
         self.fully_connected_layer(self.fc6, 128, 'fc7')
         if MODEL == 'regression':
@@ -245,3 +257,4 @@ if __name__ == '__main__':
     diff = np.expand_dims(diff, 0)
     model = NnModel(frame, diff, 1)
     model.two_stream_vgg_load()
+    print(model.t)
